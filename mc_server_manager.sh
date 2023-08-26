0f5904d4e02e2e415810cff045031253b08e90bc
Imperial-Slug <111785134+Imperial-Slug@users.noreply.github.com>
2023-08-26 14:32:27 -0400

    Added functions for centering titles and headings that appear in the program.

--- mc_server_manager.sh
+++ mc_server_manager.sh
@@ -6,8 +6,112 @@
 
 DATA_FILE=./conf/data
 set -euo pipefail
+main_menu="
+	
+	     ============================================
+            |  BASH MINECRAFT SERVER MANAGER: MAIN MENU  |
+	     ============================================
+	
+	Welcome to the bash terminal Minecraft server manager.
+	Enter one of the options listed below.
+	
+	1) Launch a Minecraft server.
+	
+	2) Install a new Minecraft server.
+	
+	3) Check my Minecraft servers for updates.
+	
+	4) Configure automatic server updates.
+	
+	5) Add an existing local Minecraft Server to BASH MINECRAFT MANAGER.
+	
+	6) Minecraft Server Administration
+	
+	\n\n"
 
-# If the datafile exists already in the script's conf directory, then proceed with normal startup.
+
+no_servers_message=" 
+It looks like you don't have any Minecraft servers configured.  To add a server to Bash MC Server Manager, exit this dialogue with any key; \n then you can either press 2 to install a new Minecraft server and add it to this program, or press 5 to add an existing Minecraft server.
+ "
+
+create_line_to_size() {
+
+c=1              
+while [[ $c -le $COLUMNS ]]; do
+    printf "="
+    ((c=c+1)) 
+done
+
+}
+
+help_to_centre() {
+# Get the difference in length between the menu item and the current 
+# terminal window, and divide it by 2 so the function can print 
+# the appropriate amount of spaces to centre the item.
+
+space_length=$(( (COLUMNS - MENU_LENGTH) / 2 ))
+
+
+}
+
+create_data_file() {
+
+printf "The datafile didn't exist.  Created the datafile in ./conf. \n"
+    	mkdir -p ./conf
+    	touch ./conf/data
+    	now=$(date)
+    	printf "# Configuration file created on $now\n  no_servers_stored=1" > ./conf/data
+	printf "Created conf directory with data file."
+	source $DATA_FILE
+
+}
+
+
+print_space_to_centre() {
+
+i=0              
+while [[ $i -le $space_length ]]; do
+    printf " "
+    ((i=i+1)) 
+done
+
+}
+
+
+option_5() {
+ 
+clear 
+MENU_LENGTH=39
+help_to_centre
+
+print_space_to_centre
+printf "=======================================
+"
+
+print_space_to_centre
+
+printf "| ADD EXISTING LOCAL MINECRAFT SERVER |
+"
+
+print_space_to_centre
+
+printf "=======================================
+"
+
+
+printf "
+Please specify the filepath of the local 
+Minecraft server you would like to add: 
+"
+   
+   create_line_to_size
+
+ read -p "
+ New Server File Path:  "
+
+}
+
+
 
 execute_menu_option() {
  
@@ -30,7 +134,7 @@ execute_menu_option() {
     ;;
 
   5)
-    echo -n "5"
+    option_5
     ;;
   
   6)
@@ -52,14 +156,7 @@ if [ -f "$DATA_FILE" ]; then
     printf "$DATA_FILE exists.\n"
     source $DATA_FILE
     
-    
-else printf "The datafile didn't exist.  Created the datafile in ./conf. \n"
-    	mkdir -p ./conf
-    	touch ./conf/data
-    	now=$(date)
-    	printf "# Configuration file created on $now\n  no_servers_stored=1" > ./conf/data
-	printf "Created conf directory with data file."
-	source $DATA_FILE
+else create_data_file
 fi
 }
 
@@ -77,29 +174,8 @@ fi
 	
 
    display_main_menu() {
-  
-	printf "
-	
-	     ============================================
-            |  BASH MINECRAFT SERVER MANAGER: MAIN MENU  |
-	     ============================================
-	
-	Welcome to the bash terminal Minecraft server manager.
-	Enter one of the options listed below.
-	
-	1) Launch a Minecraft server.
-	
-	2) Install a new Minecraft server.
-	
-	3) Check my Minecraft servers for updates.
-	
-	4) Configure automatic server updates.
-	
-	5) Add an existing Minecraft Server to BASH MINECRAFT MANAGER.
-	
-	6) Minecraft Server Administration
-	
-	\n\n"
+   
+        clear && printf %b "$main_menu"
 	
 	receive_menu_option
 	
@@ -108,7 +184,7 @@ fi
     
 	
 # ======================================= EXECUTION ==================================================
-
+	
 	check_for_config 
 	
 	# If the last command passed exited with status of 0...
@@ -117,14 +193,11 @@ fi
 	then printf '%b\n' " Configuration check complete.  Ready to go."
 	
 	if [[ $no_servers_stored -eq 1 ]]
-	then read -n 1 -s -p " ==========================================================================
-	
-It looks like you don't have any Minecraft servers configured.  To add a server to Bash MC Server Manager, exit this dialogue with any key; \n then you can either press 2 to install a new Minecraft server and add it to this program, or press 5 to add an existing Minecraft server.
-
- ========================================================================== "
+	then read -n 1 -s -p "$no_servers_message"
 	fi
 	
 	display_main_menu
+	
 	else printf " Error checking for configuration file!\n "
 	read -p -n 1 -s "Press any button to exit.\n "
 	fi
